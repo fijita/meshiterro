615e07d53b94cc219da5fbe63f0cfd81e7ef1e59
fiji <mentaiko913@iclou.com>
2021-07-31 10:14:15 +0000

    [Finish]meshiterro

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'f2206e4a6fcf78b0405a506772012dee45a82d8a5766436c22e954a3722db81a8ef4ff03d2db00edd0ba3cc8586c1dca77dec008a4484c89b49ddf2bc2ddd108'
+
